ea11e70fc81938b9b8c0f2bd1ae9d6f4958d3feb
Peter Smeets <p.e.smeets@gmail.com>
2020-04-19 13:00:32 +0200

    reformated test

--- conftest.py
+++ conftest.py
@@ -9,10 +9,3 @@ from app import create_app
 def app() -> Flask:
     app = create_app()
     return app
-
-
-@pytest.fixture  # type: ignore
-def client(app: Flask) -> FlaskClient:  # type: ignore
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
