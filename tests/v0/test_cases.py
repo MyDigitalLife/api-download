674c37299b095ee090a0a005b1d9e8f05dfd9765
Peter Smeets <p.e.smeets@gmail.com>
2020-04-19 10:40:09 +0200

    Added context to fix pipeline

--- tests/v0/test_cases.py
+++ tests/v0/test_cases.py
@@ -3,25 +3,28 @@ from uuid import uuid4, UUID
 import json
 
 
-def test_index_returns_stream_for_existing_uuid(client):
-    # TODO: make sure this passed UUID is always recent and exists
-    res = client.get(
-        url_for("v0.cases.index", uuid=UUID("37ef60df-2183-428d-b651-4c3f3cd94ebb"))
-    )
-    assert res.status_code == 200
-    assert res.is_streamed
+def test_index_returns_stream_for_existing_uuid(app, client):
+    with app.app_context():
+        # TODO: make sure this passed UUID is always recent and exists
+        res = client.get(
+            url_for("v0.cases.index", uuid=UUID("37ef60df-2183-428d-b651-4c3f3cd94ebb"))
+        )
+        assert res.status_code == 200
+        assert res.is_streamed
 
 
-def test_index_returns_stream_for_random_uuid(client):
-    res = client.get(url_for("v0.cases.index", uuid=uuid4()))
-    assert res.status_code == 200
-    assert res.is_streamed
+def test_index_returns_stream_for_random_uuid(app, client):
+    with app.app_context():
+        res = client.get(url_for("v0.cases.index", uuid=uuid4()))
+        assert res.status_code == 200
+        assert res.is_streamed
 
 
-def test_index_fails_without_args(client):
-    res = client.get(url_for("v0.cases.index"))
-    assert res.status_code == 400
-    assert json.dumps(json.loads(res.data), sort_keys=True) == json.dumps(
-        {"code": 400, "message": "No valid UUID for the requested query",},
-        sort_keys=True,
-    )
+def test_index_fails_without_args(app, client):
+    with app.app_context():
+        res = client.get(url_for("v0.cases.index"))
+        assert res.status_code == 400
+        assert json.dumps(json.loads(res.data), sort_keys=True) == json.dumps(
+            {"code": 400, "message": "No valid UUID for the requested query",},
+            sort_keys=True,
+        )
